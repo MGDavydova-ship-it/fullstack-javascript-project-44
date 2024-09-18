c00cfdfa7abadd139e59330753136f863aa848dd
MGDavydova-ship-it <m_davydova@inbox.ru>
2024-09-18 16:39:11 +0300

    Step 3: Change brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,6 @@
 #!/usr/bin/env node
 
-console.log("Welcome to the Brain Games!")
+import readlineSync from 'readline-sync';
+
+console.log("Welcome to the Brain Games!");
+const name = readlineSync.question('Your answer: ');
